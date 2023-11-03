07e1a79290acefc3abf51dd4a44e0e50c99aa72b
Kozy-Korpesh Tolep <kozykorpesh.tolep@nu.edu.kz>
2023-11-03 17:44:40 +0100

    analytics.js blog service test

---
+++ website/blog.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Get all the service elements
+    const serviceElements = document.querySelectorAll('.blog-post');
+
+    // Add a click event listener to each service element
+    serviceElements.forEach(function(service) {
+        service.addEventListener('click', function() {
+        // Get the title text within the clicked service element
+        const title = service.querySelector('h3').textContent;
+
+        // Print the title to the console
+        console.log('Clicked Service Title: ' + title);
+        var properties = {
+            title: title
+        }
+        analytics.track("Blog Post Clicked", properties);
+
+        });
+    });
+});
